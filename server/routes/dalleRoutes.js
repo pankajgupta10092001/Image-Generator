7c8709a23f07418120638ee6c257e86d8b0d3e5f
pankajgupta10092001 <pankajgupta10092001@gmail.com>
2023-09-15 12:00:13 +0530

    models and routes created

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,20 @@
+import  express from "express";
+import * as dotenv from 'dotenv';
+import  OpenAI from 'openai';
+
+
+
+dotenv.config();
+
+
+const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+
+  router.route('/').get((req,res)=>{
+    res.send('Hello from DALL-E!');
+  })
+
+export default router;
